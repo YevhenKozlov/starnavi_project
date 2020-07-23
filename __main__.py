82d2e65eb97171ba538b515d2c99aed148b961a1
Yevhen Kozlov <y@gmail.com>
2020-07-23 19:09:28 +0300

    Start point

---
+++ __main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from flask import Flask
+
+# Set start dirs
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Other imports
+from core import MainConfig, Initialization
+from controllers import MainController
+
+# Initialization
+Initialization.start_initialization()
+app = Flask(__name__)
+
+# Routes
+app.add_url_rule('/test/', 'test', MainController.test, methods=['GET'])
+
+
+if __name__ == '__main__':
+    config = MainConfig()
+    app.run(host=config.server_host, port=int(config.server_port))
