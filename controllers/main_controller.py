0997c5647e941d11fa7165dbe7721ca9b73682ea
Yevhen Kozlov <y@gmail.com>
2020-07-24 13:17:35 +0300

    Controller in progress

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -2,6 +2,7 @@ import json
 
 from hashlib import md5
 from flask import request
+from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 
 from json_objects import response_object
@@ -61,7 +62,95 @@ class MainController:
             if user.password != password:
                 raise Exception('wrong password')
 
-            result['data'] = {'access_token': user.get_token()}
+            result['data'] = {
+                'access_token': user.get_token()
+            }
+
+        except Exception as e:
+            result['success'] = False
+            result['message'] = str(e)
+
+        finally:
+            db_session.close()
+
+        return json.dumps(result, ensure_ascii=False)
+
+    @staticmethod
+    def create_post():
+        """
+        Creating new post
+        """
+
+        # TODO: Need write this method
+
+        pass
+
+    @staticmethod
+    def like_post():
+        """
+        Save like
+        """
+
+        # TODO: Need write this method
+
+        pass
+
+    @staticmethod
+    def unlike_post():
+        """
+        Save unlike
+        """
+
+        # TODO: Need write this method
+
+        pass
+
+    @staticmethod
+    def user_activity(user_id: int):
+        """
+        Getting last login time in system & last action time
+        """
+
+        result = response_object.copy()
+        db_session = DatabaseSession()
+
+        try:
+            user = db_session.query(User).filter_by(id=user_id).one()
+
+            last_login_time = user.last_login_time
+            last_action_time = user.get_last_action_time()
+
+            response_last_login_time = str(last_login_time).split(' ')[0] if last_login_time else None
+            response_last_action_time = str(last_action_time).split(' ')[0] if last_action_time else None
+
+            result['data'] = {
+                'last_login_time': response_last_login_time,
+                'last_action_time': response_last_action_time,
+            }
+
+        except Exception as e:
+            result['success'] = False
+            result['message'] = str(e)
+
+        finally:
+            db_session.close()
+
+        return json.dumps(result, ensure_ascii=False)
+
+    @staticmethod
+    def analytics(date_from: str, date_to: str):
+        """
+        Getting analytics likes|unlikes by date range
+        """
+
+        result = response_object.copy()
+        db_session = DatabaseSession()
+
+        try:
+            date_from = datetime.strptime(date_from, '%Y-%m-%d')
+            date_to = datetime.strptime(date_to, '%Y-%m-%d')
+
+            # TODO: query to DB
 
         except Exception as e:
             result['success'] = False
